8fc7160e72d69a305dee38ea0fe3f9e65a342225
--global <--global>
2025-10-24 17:24:41 +0300

    Lb7: add theme tokens, import tailwind before main.scss

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,19 @@ import { defineConfig } from "vite";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import handlebars from "vite-plugin-handlebars";
-import tailwindcss from "tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     handlebars({
       partialsDirectory: resolve(__dirname, 'src', 'partials'),
       context: { siteName: "Lab Project", labName: "Лабораторна робота №6" },
       reloadOnPartialChange: true
     }),
   ],
-  resolve: {alias: {'@': resolve(__dirname, 'src')}},
+  resolve: { alias: { '@': resolve(__dirname, 'src') } },
   build: {
     rollupOptions: {
       input: {
